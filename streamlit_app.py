cfcc54a254239c600ea26b295b7e8c36be847cd9
Eren <109238974+ErenKozak@users.noreply.github.com>
2025-03-28 06:20:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,7 @@ st.title('🎈 Machine Learning App')
 
 st.info("This is a machine learning app")
 
-df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-df
+with st.expender("Data"):
+  st.write("**Raw data**")
+  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+  df
